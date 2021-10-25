8121d2fcd5b0f5ce880904ecd2c4fad5e382ee8d
Matt <matthew.schultz1992@gmail.com>
2021-10-25 14:59:05 +1000

    First commit part 2

--- app/assets/javascripts/gameplay.js
+++ app/assets/javascripts/gameplay.js
@@ -2,70 +2,70 @@
 
 $(document).on('turbolinks:load', function() {
 
-  let team1runs = 0;
-  let team1wickets = 0;
-  let team1overs = 0;
-  let team1totalballs = 0;
-  let team2runs = 0;
-  let team2wickets = 0;
-  let team2overs = 0;
-  let team2totalballs = 0;
-  let player1turn = true;
-  
+  let team1Runs = 0;
+  let team1Wickets = 0;
+  let team1Overs = 0;
+  let team1TotalBalls = 0;
+  let team2Runs = 0;
+  let team2Wickets = 0;
+  let team2Overs = 0;
+  let team2TotalBalls = 0;
+  let player1Turn = true;
+
   const setScorecard = function () {
-    $('#team1runs').text('Runs : ' + team1runs);
-    $('#team2runs').text('Runs : ' + team2runs);
-    $('#team1wickets').text('Wickets : ' + team1wickets);
-    $('#team2wickets').text('Wickets : ' + team2wickets);
-    $('#team1overs').text('Overs : ' + team1overs);
-    $('#team2overs').text('Overs : ' + team2overs);
+    $('#team1runs').text('Runs : ' + team1Runs);
+    $('#team2runs').text('Runs : ' + team2Runs);
+    $('#team1wickets').text('Wickets : ' + team1Wickets);
+    $('#team2wickets').text('Wickets : ' + team2Wickets);
+    $('#team1overs').text('Overs : ' + team1Overs);
+    $('#team2overs').text('Overs : ' + team2Overs);
   }
 
   const bowlBallTeam1 = function() {
     ballResult = Math.floor(Math.random() * 6) + 1
     if (ballResult !== 5) {
-      team1runs += ballResult;
+      team1Runs += ballResult;
     } else {
-      team1wickets += 1;
+      team1Wickets += 1;
     }
-    team1totalballs += 1;
+    team1TotalBalls += 1;
   }
 
   const bowlBallTeam2 = function() {
     ballResult = Math.floor(Math.random() * 6) + 1
     if (ballResult !== 5) {
-      team2runs += ballResult;
+      team2Runs += ballResult;
     } else {
-      team2wickets += 1;
+      team2Wickets += 1;
     }
-    team2totalballs += 1;
+    team2TotalBalls += 1;
   }
 
   const setOvers = function (){
-    if (player1turn === true) {
-      let overs = Math.floor(team1totalballs / 6)
-      let balls = team1totalballs % 6
-      team1overs = (`${overs}.${balls}`)
+    if (player1Turn === true) {
+      let overs = Math.floor(team1TotalBalls / 6)
+      let balls = team1TotalBalls % 6
+      team1Overs = (`${overs}.${balls}`)
     } else {
-      let overs = Math.floor(team2totalballs / 6)
-      let balls = team2totalballs % 6
-      team2overs = (`${overs}.${balls}`)
+      let overs = Math.floor(team2TotalBalls / 6)
+      let balls = team2TotalBalls % 6
+      team2Overs = (`${overs}.${balls}`)
     }
   }
 
   const checkResult = function() {
-    if (team1wickets === 10 && team2wickets === 10) {
+    if (team1Wickets === 10 && team2Wickets === 10) {
       $('#result').text('Team 1 wins')
-    } else if (team2runs > team1runs) {
+    } else if (team2Runs > team1Runs) {
       $('#result').text('Team 2 wins')
     }
   }
 
   $('#bowl').on('click', function() {
-    if (player1turn === true && team1wickets < 10) {
+    if (player1Turn === true && team1Wickets < 10) {
       bowlBallTeam1();
-    } else if (team2wickets <10 && team2runs <= team1runs) {
-      player1turn = false;
+    } else if (team2Wickets <10 && team2Runs <= team1Runs) {
+      player1Turn = false;
       bowlBallTeam2();
     }
     setOvers();
